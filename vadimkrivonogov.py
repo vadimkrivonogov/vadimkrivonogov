c321df446595d318821de1d7e312a9362d65bdb7
vadimkrivonogov <120089193+vadimkrivonogov@users.noreply.github.com>
2022-12-14 14:13:03 +0200

    Update vadimkrivonogov.py

--- vadimkrivonogov.py
+++ vadimkrivonogov.py
@@ -1,48 +1,52 @@
+from ast import Try
+from ctypes.wintypes import INT
 from math import *
+from pickletools import int4
 from random import *
+from tkinter.tix import InputOnly
 
 
 
 #1
-print("Puu l‰bimııdu arvutamine")
-C=float(input("Puu ¸mbermııt: "))
+print("Puu l√§bim√µ√µdu arvutamine")
+C=float(input("Puu √ºmberm√µ√µt: "))
 d=2*(C/(2*pi))
-print(f"Vastus:\nPuu l‰bimııduga {C} ¸mbermııt vırdub {d}")
+print(f"Vastus:\nPuu l√§bim√µ√µduga {C} √ºmberm√µ√µt v√µrdub {d}")
 
 #2
-print("Ristk¸likukujulise maat¸ki diagonaal")
-N=float(input("Sisesta ristk¸liku 1. k¸lje pikkus => "))
-M=float(input("Sisesta ristk¸liku 2. k¸lje pikkus => "))
+print("Ristk√ºlikukujulise maat√ºki diagonaal")
+N=float(input("Sisesta ristk√ºliku 1. k√ºlje pikkus => "))
+M=float(input("Sisesta ristk√ºliku 2. k√ºlje pikkus => "))
 d=sqrt(N**2+M**2)
-print(f"Maat¸ki diagonaal on {d} m**2")
+print(f"Maat√ºki diagonaal on {d} m**2")
  
 
 #3
 
 print("Ruudu karakteristikud")
-a=float(input("Sisesta ruudu k¸lje pikkus => "))
+a=float(input("Sisesta ruudu k√ºlje pikkus => "))
 S=a**2
 print("Ruudu pindala", round(S,1))
 P=4*a
-print("Ruudu ¸mbermııt", round(P,1))
+print("Ruudu √ºmberm√µ√µt", round(P,1))
 di=a*sqrt(2)
 print("Ruudu diagonaal", round(di,2))
 print()
 #-----------------------------
-print("Ristk¸liku karakteristikud")
-b=float(input("Sisesta ristk¸liku 1. k¸lje pikkus => "))
-c=float(input("Sisesta ristk¸liku 2. k¸lje pikkus => "))
+print("Ristk√ºliku karakteristikud")
+b=float(input("Sisesta ristk√ºliku 1. k√ºlje pikkus => "))
+c=float(input("Sisesta ristk√ºliku 2. k√ºlje pikkus => "))
 S=b*c
-print("Ristk¸liku pindala", round(S,1))
+print("Ristk√ºliku pindala", round(S,1))
 P=2*(b+c)
-print("Ristk¸liku ¸mbermııt", round(P,1))
+print("Ristk√ºliku √ºmberm√µ√µt", round(P,1))
 di=sqrt(b*2+c*2)
-print("Ristk¸liku diagonaal", round(di,1))
+print("Ristk√ºliku diagonaal", round(di,1))
 print()
 print("Ringi karakteristikud")
 r=float(input("Sisesta ringi raadiusi pikkus => "))
 d=2*r
-print("Ringi l‰bimııt", round(d,1))
+print("Ringi l√§bim√µ√µt", round(d,1))
 S=pi*r**2
 print("Ringi pindala", round(S,2))
 C=2*pi*r
@@ -53,27 +57,27 @@ print("Ringjoone pikkus", round(C,2))
 try:
     A1=int(input("Sisesta 1. arv => "))    
 except :
-    print("Vale andmet¸¸p!")
+    print("Vale andmet√º√ºp!")
     A1=0
 try:
     A2=int(input("Sisesta 2. arv => "))    
 except :
-    print("Vale andmet¸¸p!")
+    print("Vale andmet√º√ºp!")
     A2=0
 try:
     A3=int(input("Sisesta 3. arv => "))    
 except :
-    print("Vale andmet¸¸p!")
+    print("Vale andmet√º√ºp!")
     A3=0
 try:
     A4=int(input("Siseta4. arv => "))
 except :
-    print("Vadle andmet¸¸p!")
+    print("Vadle andmet√º√ºp!")
     A4=0
 try:
     A5=int(input("Sisesta 5. arv => "))    
 except :
-    print("Vale andmet¸¸p!")
+    print("Vale andmet√º√ºp!")
     A5=5
 Keskmine=(A1+A2+A3+A4+A5)/5
 print(f"Keskmine on {Keskmine}")
@@ -88,24 +92,24 @@ print("^^ "'""'" ^^ ")
 a=randint(1,100)
 b=randint(1,100)
 c=randint(1,100)
-print(f"K¸lg a={a}\nK¸lg b={b}\nK¸lg c={c}")
-print(f"Kolmnurga ¸mbermııt = {a+b+c}")
+print(f"K√ºlg a={a}\nK√ºlg b={b}\nK√ºlg c={c}")
+print(f"Kolmnurga √ºmberm√µ√µt = {a+b+c}")
 print()
 
 #7
-print("Pitsa Vıtsite 3 sıbraga suure pitsa hinnaga 12,90Ä te j‰tate teenindajale 10% jootraha")
+print("Pitsa V√µtsite 3 s√µbraga suure pitsa hinnaga 12,90‚Ç¨ te j√§tate teenindajale 10% jootraha")
 s=10*12.90/100
 s=round(s)
 d=(12.90+s)
 p=d/3
 p=round(p,1)
-print (f"Iga sıber peab maksma: {p}Ä")
+print (f"Iga s√µber peab maksma: {p}‚Ç¨")
 print()
 
 #8
-print("K¸tusekulu arvutamine")
-l=float(input("Kasutaja sisestab tangitud k¸tuse liitrid: "))
-km=float(input("Kasutaja sisestab l‰bitud kilomeetrid: "))
+print("K√ºtusekulu arvutamine")
+l=float(input("Kasutaja sisestab tangitud k√ºtuse liitrid: "))
+km=float(input("Kasutaja sisestab l√§bitud kilomeetrid: "))
 p=l/km*100
 print (f"Vastus: {p}l/km")
 print()
@@ -139,7 +143,7 @@ if r>0 and a>0:
     elif Skr>Skv:
         print(f"Ruudu pindala {Skr} m^^2 on suurem ringi pindala {Skv} m^2")
     else:
-         print("Pindalad on vırised. {Skr} m^2")
+         print("Pindalad on v√µrised. {Skr} m^2")
 else:
     print(f"(a) ja (r) paevad > kui 0 olla")
 print()
@@ -148,8 +152,8 @@ print()
 from math import *
 from random import * 
 
-#M‰‰rata ja tuletada suurem kahest sisestatavast arvust.
-print("M‰‰rata ja tuletada suurem kahest sisestatavast arvust")
+#M√§√§rata ja tuletada suurem kahest sisestatavast arvust.
+print("M√§√§rata ja tuletada suurem kahest sisestatavast arvust")
 a=randint(-100,100)
 b=randint(-100,100)
 print(f"a={a}\nb={b}")
@@ -166,9 +170,9 @@ print("Isikukoodi kontrollimine")
 text=input("Palun kirjuta teie isikukod: ")
 n=len(text)
 if n==11 and text.isdigit(): 
-    print("Teie hagikood on sisestatud ıigesti")
+    print("Teie hagikood on sisestatud √µigesti")
 else:
-    print("iskukoodis lubamatud v‰‰rtused!")
+    print("iskukoodis lubamatud v√§√§rtused!")
 
 print()
 
@@ -185,29 +189,29 @@ if r>0 and a>0:
     elif Skr>Skv:
        print(f"Rindi pindala {Skr} m^2 on suurem ruudu pindala {Skv} m^2")
     else:
-       print("Pindala ov vırsed. {Skr} m^2")
+       print("Pindala ov v√µrsed. {Skr} m^2")
 else:
     print(f"{a} ja {r} peavad > kui 0 olla")
 print()
 
 
-print("N‰dalap‰evad")
+print("N√§dalap√§evad")
 try:
-    p=int(input("Mis p‰ev t‰na on?"))
+    p=int(input("Mis p√§ev t√§na on?"))
     if   p==1:
-        n="Esmasp‰ev"
+        n="Esmasp√§ev"
     elif p==2:
-        n="Teisip‰ev"
+        n="Teisip√§ev"
     elif p==3:
-        n="Kolmap‰ev"
+        n="Kolmap√§ev"
     elif p==4:
-        n="Neljap‰ev"
+        n="Neljap√§ev"
     elif p==5:
         n="Reedel"
     elif p==6:
-        n="P¸hap‰ev"
+        n="P√ºhap√§ev"
     elif p==7:
-        n="Laup‰ev"
+        n="Laup√§ev"
     else:
         n="Vale Number"  
     print(n)
@@ -217,42 +221,59 @@ except:
 
 #2
 try:
-    p‰ev=int(input("Mis p‰ev t‰na on?"))
+    p√§ev=int(input("Mis p√§ev t√§na on?"))
 except:
     print("!!!!!!")
-if p‰ev==1:
-    print("Esmasp‰ev")
-elif p‰ev==2:
-    print("Teisip‰ev")
-elif p‰ev==3:
-    print("Kolmap‰ev")
-elif p‰ev==4:
-    print("Neljap‰ev")
-elif p‰ev==5:
+if p√§ev==1:
+    print("Esmasp√§ev")
+elif p√§ev==2:
+    print("Teisip√§ev")
+elif p√§ev==3:
+    print("Kolmap√§ev")
+elif p√§ev==4:
+    print("Neljap√§ev")
+elif p√§ev==5:
     print("Reedel")
-elif p‰ev==6:
-    print("P¸hap‰ev")
-elif p‰ev==7:
-    print("Laup‰ev")
+elif p√§ev==6:
+    print("P√ºhap√§ev")
+elif p√§ev==7:
+    print("Laup√§ev")
 else:
     print("Viga!")
 
 
 #1
 try:
-    hinne=int(input("Mis hinne t‰na said koolid"))
+    hinne=int(input("Mis hinne t√§na said koolid"))
 except:
     print("!!!!")
 if hinne==5:
-    print("v‰ga hea")
+    print("v√§ga hea")
 elif hinne==4:
     print("Hea!")
 elif hinne==3:
     print("Rahuldav")
-elif hinne==2 or hinne==1: #and, or,not !=ei vırdu,<,>,>=,<=
+elif hinne==2 or hinne==1: #and, or,not !=ei v√µrdu,<,>,>=,<=
     print("Mitte rahuldav") 
 else:
     print("Viga!")
 
 #14.12.22
+try:
+    vanus=int(Input("Kui vana sa oled?"))
+    if vanus>18:
+        print("Kas te annate vanematele loa oma Thavelit vaadata?")
+        o=(input("Jah v√µi ei."))
+        if o.lower()=="jah": #upper()
+             print((o))
+             print("See on ligip√§√§s teie vanematele.")
+             print("Tahvel on kinni.")
+        elif o.upper()=="E1":
+            print("Sissep√§√§s puudub.")
+            print("Tahvel on kinni.")
+    if vanus<18:
+        print
+   
+
+    
  
